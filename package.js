b8da51050dc895f186aee602e9cd19c92fd288a2
Luigi Maselli <grigio.org@gmail.com>
2015-02-16 00:30:21 +0100

    bump 0.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'grigio:babel',
   summary: 'Write javascript ES6 in your Meteor app',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/grigio/meteor-babel.git'
 });
 
@@ -19,7 +19,7 @@ Package.registerBuildPlugin({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.0.3.1');
 
   // runtime
   api.addFiles('lib/runtime.js');
