4f1fb500e7b2715103a56f51ea873991f59eb3c5
Luigi Maselli <grigio.org@gmail.com>
2015-02-28 17:35:04 +0100

    bump 0.0.6

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'grigio:babel',
   summary: 'Write javascript ES6 in your Meteor app',
-  version: '0.0.5',
+  version: '0.0.6',
   git: 'https://github.com/grigio/meteor-babel.git'
 });
 
