17ac6bd7173472d330cdf63b58acd7aaa187a2fa
Luigi Maselli <grigio.org@gmail.com>
2015-07-24 12:56:09 +0200

    updated to babel 5.8.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'grigio:babel',
   summary: 'Write javascript ES6 in your Meteor app',
-  version: '0.1.5',
+  version: '0.1.6',
   git: 'https://github.com/grigio/meteor-babel.git'
 });
 
@@ -13,7 +13,7 @@ Package.registerBuildPlugin({
     'plugin/compile-6to5.js'
   ],
   npmDependencies: {
-                      'babel-core':'5.6.14'
+                      'babel-core':'5.8.3'
                    }
 });
 
